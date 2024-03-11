b46c9b6bbc940291815831073bfa1137a66c188f
Mohamed Aboalmkarem <mo3ddeedd67983@gmail.com>
2024-03-11 04:42:21 +0200

    start css in search

---
+++ search/search.js
@@ -0,0 +1,14 @@
+
+
+// function getResult() {
+//     let countrySelect = document.getElementById('countrySelect')
+//     countrySelect.val
+// }
+// let countrySelect = document.getElementById('countrySelect')
+// countrySelect.addEventListener('change', () => {
+//     console.log(this.countrySelect.value)
+//     document.getElementById('result').innerHTML = `
+//         <h1>${this.countrySelect.value}</h1>
+//     `
+// })
+
